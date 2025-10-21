c793763bcdd47d55233545c39bb02c5672efbd59
Maiki <maikif37@outlook.com>
2025-10-21 01:09:21 -0300

    Contador com While

---
+++ ContadorWhile.java
@@ -0,0 +1,10 @@
+public class ContadorWhile {
+    public static void main(String[] args) {
+        int contador = 1;
+
+        while (contador <= 12) {
+            System.out.printf("contador: %d%n", contador);
+            contador++;
+        }
+    }
+}
