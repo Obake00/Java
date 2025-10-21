8b154e101ef206b3b71ad1dfb532ecbef33416d2
Maiki <maikif37@outlook.com>
2025-10-21 01:12:23 -0300

    ContadorFor

---
+++ ContadorFor.java
@@ -0,0 +1,7 @@
+public class ContadorFor {
+    public static void main(String[] args) {
+        for (int contador = 1; contador <= 12; contador++) {
+            System.out.printf("contador: %d%n", contador);
+        }
+    }
+}
