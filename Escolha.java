01407ea38fa5bda87a784133d6de8e0f57d8c2bf
Maiki <maikif37@outlook.com>
2025-10-21 01:18:57 -0300

    escolha if

---
+++ Escolha.java
@@ -0,0 +1,12 @@
+public class Escolha {
+    public static void main(String[] args) {
+        int opcao = 2;
+        if (opcao == 1) {
+            System.out.printf("O numero %d e impar", opcao);
+        }
+        else {
+            System.out.printf("O numero %d e par", opcao);
+        }
+    }
+    
+}
