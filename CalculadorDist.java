05f04eca7c0e4be14b5e42d1c211763a42a0b9e5
Maiki <maikif37@outlook.com>
2026-01-26 22:18:09 -0300

    calc ajust

--- CalculadorDist.java
+++ CalculadorDist.java
@@ -8,10 +8,10 @@ void main(){
     float vl = tc.nextFloat();
 
     System.out.println("--- VAMOS AS PORCENTAGENS (%) ---");
-    System.out.println(" 1° parte 30% -> " + vl*0.3);
-    System.out.println(" 2° parte 30% -> " + vl*0.3);
-    System.out.println(" 3° parte 15% -> " + vl*0.15);
-    System.out.println(" 4° parte 15% -> " + vl*0.15);
-    System.out.println(" 5° parte 10% -> " + vl*0.1);
+    System.out.println(" 1° parte 30% -> R$" + vl*0.3);
+    System.out.println(" 2° parte 30% -> R$" + vl*0.3);
+    System.out.println(" 3° parte 15% -> R$" + vl*0.15);
+    System.out.println(" 4° parte 15% -> R$" + vl*0.15);
+    System.out.println(" 5° parte 10% -> R$" + vl*0.1);
     System.out.println("---------------------------------");
 }
